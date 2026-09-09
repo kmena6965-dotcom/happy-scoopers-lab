9292e8874b7445856c0ffe5a42961e07cc55fc5a
kmena6965-dotcom <kmena6965@gmail.com>
2026-09-09 10:40:33 -0600

    Agrega modelo stg_oltp__products.sql

---
+++ transform/models/staging/stg_oltp__products.sql
@@ -0,0 +1,6 @@
+with source as ( select * from {{ source('raw', 'products') }} )
+select
+    product_id, product_name, product_code, product_description,
+    subcategory_id, unit_of_measure_id, unit_price,
+    case when discontinued then 'Sí' else 'No' end as is_discontinued
+from source
