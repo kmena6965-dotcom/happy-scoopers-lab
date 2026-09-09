da471c15fe8d41febabc609180b12685f43ec170
kmena6965-dotcom <kmena6965@gmail.com>
2026-09-09 10:52:14 -0600

    Agrega modelo stg_oltp__units_of_measure.sql

---
+++ transform/models/staging/stg_oltp__units_of_measure.sql
@@ -0,0 +1,6 @@
+with source as ( select * from {{ source('raw', 'units_of_measure') }} )
+select
+    unit_of_measure_id,
+    unit_measure_code as unit_of_measure_code,
+    name as unit_of_measure_name
+from source
